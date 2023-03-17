bf34f6479e614ff6c769c8de157d85c6ec10aff0
andiz2 <codeandi@gmail.com>
2023-03-17 12:31:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Post from "./Post";
+function App() {
+  return (
+    <main>
+      <Post author="Maximilian" body="React.js is awesome" />
+      <Post author="Manuel" body="check out the full course!" />
+      <Post />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Post.jsx
@@ -0,0 +1,12 @@
+const names = ["Maximilian", "Manuel"];
+
+function Post(props) {
+  return (
+    <div>
+      <p>{props.author}</p>
+      <p>{props.body}</p>
+    </div>
+  );
+}
+
+export default Post;
